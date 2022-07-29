626a9f57f6326d4c515c7c788b11c187c85e9a6f
21013198 <21013198@10.174.115.27>
2022-07-29 13:32:29 +0800

    template for test cases

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,103 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
-
+	
 	@Before
 	public void setUp() throws Exception {
+		// prepare test data
 	}
 
-	@After
-	public void tearDown() throws Exception {
+	
+	@Test
+	public void testAddParents() {
+		// Item list is not null, so that can add a new item - boundary
+	
+	}
+	@Test
+	public void testAddStudent() {
+	
+	}
+	
+	@Test
+	public void testRetrieveAllParent() {
+		
+	}
+	@Test
+	public void testRetrieveAllStudent() {
+		
 	}
 
 	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
+	public void testDeleteParent() {
+		
+	}
+	
+	@Test
+	public void testDeleteStudent() {
+	
+		
+	}
+	
+	@Test
+	public void testAddMenuBanks() {
+		
+	}
+	@Test
+	public void testRetrieveMenuBanksItems() {
+		
+	}
+	@test
+	public void testDeleteMenuBanks(){
+		
+	}
+	@test
+	public void testCreateMenu() {
+		
+	}
+	@test
+	public void testViewMenu() {
+		
+	}
+	@test 
+	public void testDeleteMenu() {
+		
+	}
+	@test
+	public void testAddLunchBoxOrder(){
+		
+	}
+	@test
+	public void testViewLunchBoxOrder() {
+		
+	}
+	@test
+	public void testDeleteLunchBoxOrder() {
+		
+	}
+	@test
+	public void testAddOrderBill() {
+		
+	}
+	@test
+	public void testViewOrderBill() {
+		
+	}
+	@test
+	public void testDeleteOrderBill() {
+		
+	}
+	
+	
+	@After
+	public void tearDown() throws Exception {
+
 	}
 
 }
+
