8b13419a2821d22f2988f02849cb6dbc937b11c5
21013198 <21013198@10.174.115.27>
2022-07-29 13:56:33 +0800

    Template and classes for every class

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,56 @@
 
-public class C206_CaseStudy {
+import java.util.ArrayList;
 
+public class C206_CaseStudy{// student 1 - bala
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		int option = 0;
+
+		while (option != 16) {
+
+
+			if (option == 1) {
+				
+
+			} else if (option == 2) {
+				
+			} else if (option == 3) {
+				
+			} else if (option == 4) {
+				
+			} else if (option == 5) {
+				
+			} else if (option == 6) {
+				
+			} else if (option == 7) {
+				
+			} else if (option == 8) {
+				
+			} else if (option == 9) {
+				
+			} else if (option == 10) {
+				
+			} else if (option == 11) {
+				
+			} else if (option == 12) {
+				
+			} else if (option == 13) {
+				
+			} else if (option == 14) {
+				
+			} else if (option == 15) {
+				
+			} else {
+				System.out.println("Thank you for using this programme!!");
+				
+			}
+			
+			
+		}
 
 	}
 
 }
+
